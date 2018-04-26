1f25265c6fca74abc108d0c2abd98d1adb62feb2
597 <312181653@qq.com>
2018-04-27 01:40:26 +0800

    Init configure

--- README.md
+++ README.md
@@ -5,17 +5,31 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## 慕课网imooc-react-chat.
 
 ### mongodb配置(windows)
+
 - mongodb安装目录下如：D:\mongodb 创建data\db文件夹
+
 - 创建logs\mongo.log文件
+
 - 创建mongod.conf文件，编写mongodb配置文件
+
   - dbpath=D:\mongodb\data\db  数据存放文件路径
+
   - logpath=D:\mongodb\logs\mongo.log  log文件存放路径
+
   - logappend=true  以追加的方式记录日志
+
   - port=27017  启动端口
+
   - quiet=true  过滤无用信息
+
 - 已管理员身份打开cmd(C:\Windows\System32\cmd.exe右键管理员身份打开)
+
 - cd到mongodb安装目录的bin目录下
+
 - 输入mongod --config D:\mongodb\mongod.conf --install serviceName "MongoDB",如果没有任何提示表示成功，这一步需要以管理员身份打开cmd
+
 - net start MongoDB 显示已启动
+
 - 输入mongo可以查看链接地址等信息
+   
 - net stop MongoDB 可关闭服务
--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import FastClick from 'fastclick'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import './config'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import reducers from './reducers'
 
 import App from './App'
+
+const logger = createLogger()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(
+    logger,
+    thunk
+  )
+)
 
 FastClick.attach(document.body)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App}></Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
 registerServiceWorker()
